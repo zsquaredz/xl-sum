ac270ac239615da36b0bfb825e23944a85edd3f3
zsquaredz <zhaozheng1997@hotmail.com>
2022-05-09 01:04:14 +0100

    run exp

--- multilingual_rouge_scoring/rouge_calc.py
+++ multilingual_rouge_scoring/rouge_calc.py
@@ -2,11 +2,11 @@ import argparse
 from rouge_score import rouge_scorer
 import tempfile
 
-def calc_rouge(prediction_file, reference_file):
+def calc_rouge(prediction_file, reference_file, stem, lang):
     print('calculating ROUGE')
     print('prediction file', prediction_file)
     print('reference file', reference_file)       
-    scorer = rouge_scorer.RougeScorer(['rouge1', 'rouge2', 'rougeL'], use_stemmer=True, lang="english")
+    scorer = rouge_scorer.RougeScorer(['rouge1', 'rouge2', 'rougeL'], use_stemmer=stem, lang=lang)
     scores = {'rouge1':[],'rouge2':[],'rougeL':[]}
     with open(prediction_file, encoding='utf-8') as pred_f,\
         open(reference_file, encoding='utf-8') as ref_f:
@@ -29,7 +29,7 @@ if __name__ == '__main__':
     parser.add_argument("--stem", action='store_true', help="Use stemming")
     parser.add_argument("--lang", '-l', type=str, default='english', help="Language")
     args = parser.parse_args()
-    r1, r2, rl = calc_rouge(args.prediction_file, args.reference_file)
+    r1, r2, rl = calc_rouge(args.prediction_file, args.reference_file, args.stem, args.lang)
     print(f'\t R-1: {r1} | R-2: {r2} | R-L: {rl}')
     # r1, r2, rl, rlsum = calc_rouge_hack_for_ja(args.prediction_file, args.reference_file)
     # print(f'\t R-1: {r1} | R-2: {r2} | R-L: {rl} | R-Lsum: {rlsum}')
