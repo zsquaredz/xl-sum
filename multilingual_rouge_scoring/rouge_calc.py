90aa4fe922622a18e2a2cd684db2dab4588f65f1
zsquaredz <zhaozheng1997@hotmail.com>
2022-05-09 01:03:12 +0100

    run exp

--- multilingual_rouge_scoring/rouge_calc.py
+++ multilingual_rouge_scoring/rouge_calc.py
@@ -26,6 +26,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="summarization")
     parser.add_argument("--prediction_file", '-p', type=str, default='./data/test.target', help="Location of prediction file")
     parser.add_argument("--reference_file", '-r', type=str, default='./data/test.target', help="Location of reference file")
+    parser.add_argument("--stem", action='store_true', help="Use stemming")
+    parser.add_argument("--lang", '-l', type=str, default='english', help="Language")
     args = parser.parse_args()
     r1, r2, rl = calc_rouge(args.prediction_file, args.reference_file)
     print(f'\t R-1: {r1} | R-2: {r2} | R-L: {rl}')
